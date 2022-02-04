e82afae62cf83226ca86109837c94f6fe19271d2
Sagi Korzack <Sagi1193@gmail.com>
2022-02-04 15:39:30 +0200

    update settings.py

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru8u_70bqn_2(iq0zh=(l96f=g&o(a1_6vr6@3v$zxnfh$nqbs'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = []
 
