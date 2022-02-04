c635eb8a48a5208a75a151154355c95c2ab3d39d
Sagi Korzack <Sagi1193@gmail.com>
2022-02-04 14:03:46 +0200

    model and serializers set up

--- app/movies/models.py
+++ app/movies/models.py
@@ -1,7 +1,20 @@
+from atexit import register
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib import admin
 # Create your models here.
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    year = models.CharField(max_length=4)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
 
